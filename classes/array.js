83071e22e7e4b163cd30c81c1da81a0bb8d1b273
Chinaka_Chimaobi <80992410+Chimaobi-01@users.noreply.github.com>
2025-06-22 08:29:19 +0000

    refactor to ES5 class syntax

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,13 @@
 // Your code here
+Array.prototype.isEqual = function (array) {
+    if(this.length !== array.length)
+        return false
+
+    for (let i = 0; i < this.length; i++) {
+        const element = this[i];
+        if(array[i] !== element)
+            return false
+    }
+
+    return true
+}
--- classes/dog.js
+++ classes/dog.js
@@ -1,16 +1,13 @@
-class Dog {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
 
-  bark() {
-    console.log("Bark");
-  }
-
-  static diet() {
-    return "carnivore";
-  }
+function Dog(name, age) {
+  this.name = name
+  this.age = age
+}
+Dog.prototype.bark = function () {
+  console.log("Bark")
+}
+Dog.diet = function (){
+  return "carnivore"
 }
 
 /****************************************************************************/
